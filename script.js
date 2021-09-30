fd28f188bede50578d16027a674627d7551dc33c
cd <coolezprogramming@gmail.com>
2021-09-30 18:59:25 -0400

    page setup

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const Gameboard = (() => {
+    let board = Array(9).fill('');
+})();
+
+const Player = (sign) => {
+    const sign = () => sign;
+    let win = () => false;
+};
+
+const display = (() => {})();
